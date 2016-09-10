110e0c7b0ab32993e412a4ddc8826bc09dbbce46
Camille Hamon <camilleh@ntnu.no>
2016-09-10 15:56:27 +0200

    Added a filled-in mpc structure as output

--- findmaxll.m
+++ findmaxll.m
@@ -1,4 +1,4 @@
-function results = findmaxll(mpc,idxVarPQ,dir)
+function [mpc,results] = findmaxll(mpc,idxVarPQ,dir)
 % FINDMAXLL finds the maximum loadability limit point in a particular
 % direction by using the YALMIP implementation that is formulated with the
 % voltages expressed in rectangular coordinates.
@@ -9,6 +9,8 @@ n = size(mpc.bus,1);
 mu = mpc.bus(idxVarPQ,PD);
 
 %% Scale to per-unit
+mpc0 = mpc;
+tanphi0 = mpc.bus(idxVarPQ,QD)./mpc.bus(idxVarPQ,PD);
 mpc.bus(:,[PD,QD]) = mpc.bus(:,[PD,QD])/mpc.baseMVA;
 mpc.gen(:,[PG,QG]) = mpc.gen(:,[PG,QG])/mpc.baseMVA;
 mpc.gen(:,[QMAX,QMIN]) = mpc.gen(:,[QMAX,QMIN])/mpc.baseMVA;
@@ -26,9 +28,7 @@ dirP(idxVarPQ) = dir;
 
 % optimization problem
 x = sdpvar(2*n,1);
-% v = sdpvar(2*n-1,1);
 Ploads = sdpvar(n,1);
-% assign(v,ones(2*n-1,1));
 assign(x,ones(2*n,1));
 assign(Ploads,mpc.bus(:,PD));
 lambda = sdpvar(1,1);
@@ -39,10 +39,14 @@ Constraints = [Constraints,...
 options = sdpsettings('verbose',0,'showprogress',1,'fmincon.Algorithm', 'interior-point','usex0',1);
 sol = optimize(Constraints,Objective,options);
 
-% Extracting the result
+% Extracting the result, scaling to MW
 Ploadsopt1 = value(Ploads);
 lambda = norm(Ploadsopt1(idxVarPQ)-mu);
-results.Ploads = value(Ploads);
+results.Ploads = value(Ploads)*mpc.baseMVA;
 results.x = value(x);
 results.lambda = lambda;
+% Putting the results into the original system in MVA
+mpc = mpc0; 
+mpc.bus(idxVarPQ,PD) = results.Ploads;
+mpc.bus(idxVarPQ,QD) = tanphi0.*results.Ploads;
 end
